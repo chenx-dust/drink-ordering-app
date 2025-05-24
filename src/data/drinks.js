b797ee0e86c6318f8c22c62442076e716b583a60
longlin10086 <gnulonglin@gmail.com>
2025-05-24 11:11:04 +0800

    chores: 将图片变为本地

--- src/data/drinks.js
+++ src/data/drinks.js
@@ -8,24 +8,21 @@ const drinks = [
         name: "浓缩咖啡",
         description: "通过高压热水萃取细磨咖啡豆制成的浓咖啡",
         basePrice: 28.0,
-        image:
-          "https://images.unsplash.com/photo-1520516472218-ed48f8f1d9df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        image: "/assets/images/Espresso.png",
       },
       {
         id: 102,
         name: "卡布奇诺",
         description: "浓缩咖啡搭配蒸汽牛奶和奶泡",
         basePrice: 32.0,
-        image:
-          "https://images.unsplash.com/photo-1534778101976-62847782c213?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        image: "/assets/images/Cappuccino.png",
       },
       {
         id: 103,
         name: "拿铁",
         description: "浓缩咖啡搭配蒸汽牛奶",
         basePrice: 35.0,
-        image:
-          "https://images.unsplash.com/photo-1570968915860-54d5c301fa9f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        image: "/assets/images/Latte.png",
       },
     ],
   },
@@ -38,24 +35,21 @@ const drinks = [
         name: "绿茶",
         description: "富含抗氧化剂的清新茶饮",
         basePrice: 22.0,
-        image:
-          "https://images.unsplash.com/photo-1556682851-71e037122346?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        image: "/assets/images/GreenTea.png",
       },
       {
         id: 202,
         name: "伯爵红茶",
         description: "佛手柑风味红茶",
         basePrice: 25.0,
-        image:
-          "https://images.unsplash.com/photo-1571934811356-5cc061b6821f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        image: "/assets/images/RedTea.png",
       },
       {
         id: 203,
         name: "印度奶茶",
         description: "香料茶与蒸汽牛奶混合",
         basePrice: 30.0,
-        image:
-          "https://images.unsplash.com/photo-1589488766611-b2287a046189?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        image: "/assets/images/Masala.png",
       },
     ],
   },
@@ -68,24 +62,21 @@ const drinks = [
         name: "冰咖啡",
         description: "冰镇咖啡配冰块饮用",
         basePrice: 28.0,
-        image:
-          "https://images.unsplash.com/photo-1578314675249-a6910f80cc39?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        image: "/assets/images/IceCoffee.png",
       },
       {
         id: 302,
         name: "冷萃咖啡",
         description: "冷水长时间慢速萃取的咖啡",
         basePrice: 35.0,
-        image:
-          "https://images.unsplash.com/photo-1517701604599-bb29b565090c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        image: "/assets/images/ColdCoffee.png",
       },
       {
         id: 303,
         name: "柠檬水",
         description: "鲜榨柠檬饮品",
         basePrice: 20.0,
-        image:
-          "https://images.unsplash.com/photo-1556881286-fc6915169721?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        image: "/assets/images/lemonWater.png",
       },
     ],
   },
